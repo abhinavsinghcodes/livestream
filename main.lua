50614b486204094dd75ab5c1590c02d26fffd702
Abhinav Singh <146484064+abhinavsinghcodes@users.noreply.github.com>
2024-07-03 23:30:08 +0530

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,8 @@
+1) lua is lua 
+2) data types ->
+   a) number ex: 10
+   b) String ex: "Hello"
+   c) boolean ex: a = true
+3) keywords(21) -> https://www.lua.org/manual/5.1/manual.html
+4) Operators -> https://www.lua.org/manual/5.1/manual.html
+5) Operation on operands 
